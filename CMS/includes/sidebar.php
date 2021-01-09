a1e9bfd502d17ac3a7f80f5cfee3e21344bdb724
mroloff <mroloff.dev@gmail.com>
2021-01-08 21:08:16 -0800

    Updated sidebar.php
    Changed the title of the search bar.

--- CMS/includes/sidebar.php
+++ CMS/includes/sidebar.php
@@ -8,7 +8,7 @@
 
                 <!-- Blog Search Well -->
                 <div class="well">
-                    <h4>Blog Search</h4>
+                    <h4>Search</h4>
                     <form action="search.php" method="post">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control">
