3e56f92f3f2e5ab34fd2a6edc0692ea133d1b168
mroloff <mroloff.dev@gmail.com>
2021-01-10 13:03:22 -0800

    Code refactoring categories.php
    created functions in function.php

--- CMS/admin/categories.php
+++ CMS/admin/categories.php
@@ -3,46 +3,46 @@
 
 <body>
 
-    <div id="wrapper">
+<div id="wrapper">
 
-       
-       
-       
-       
-       
-       
-       
-        <!-- Navigation -->
-        <?php include "includes/admin_navigation.php";?>
 
-        
-        
-        
-        
-        
-        
 
-        <div id="page-wrapper">
 
-            <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                       
-                       
-                       
-                       
-                        <h1 class="page-header">
-                            Welcome to admin
-                            <small>Author</small>
-                        </h1>
-                        
-                        <div class="col-xs-6">
-                        
+
+
+
+<!-- Navigation -->
+<?php include "includes/admin_navigation.php";?>
+
+
+
+
+
+
+
+
+<div id="page-wrapper">
+
+<div class="container-fluid">
+
+<!-- Page Heading -->
+<div class="row">
+<div class="col-lg-12">
+
+
+
+
+<h1 class="page-header">
+Welcome to admin
+<small>Author</small>
+</h1>
+
+<div class="col-xs-6">
+
 <?php insert_categories(); ?>
-                                       
-                        
+
+
 <form action="" method="post">
 <div class="form-group">
 <label for="cat_title">Add Category</label>
@@ -53,31 +53,31 @@
 </div>
 
 </form>
-                            
 
-<?php 
- if(isset($_GET['edit'])) {
-  $cat_id = $_GET['edit'];
-    
+
+<?php //UPDATE AND INCLUDE QUERY
+if(isset($_GET['edit'])) {
+$cat_id = $_GET['edit'];
+
 include "includes/update_categories.php";
-     
-     
-     
-     
- }                           
-                            
-                            
-                            
-?>                    
-                            
-                            
-                            
 
-                            
+
+
+
+}                           
+
+
+
+?>                    
+
+
+
+
+
 </div><!--  Add Category form-->
 
 <div class="col-xs-6">
-    
+
 <table class="table table-boardered table-hover" >
 <thead>
 <tr>
@@ -86,63 +86,33 @@ include "includes/update_categories.php";
 </tr>
 </thead>
 <tbody>
-             
-
-<?php  //find all categories query
-$query = "SELECT * FROM categories";
-$select_categories = mysqli_query($connection,$query);
-
-                               
-while($row = mysqli_fetch_assoc($select_categories)) {
-$cat_id = $row['cat_id'];
-$cat_title = $row['cat_title']; 
-
-echo "<tr>";
-echo "<td>{$cat_id}</td>";
-echo "<td>{$cat_title}</td>";
-echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-echo "<tr>";
-
-}
-
-?> 
-
-                          
-                          
-<?php 
-//Delete QUERY
-
-if(isset($_GET['delete'])) {
 
 
-$the_cat_id = $_GET['delete'];
+<!-- Find all categories-->
+<?php findAllCategories();?>
 
-$query = "DELETE FROM categories WHERE cat_id={$the_cat_id}";
-$delete_query = mysqli_query($connection, $query);
-header("Location: categories.php"); //refreshes page
+<!--Delete QUERY-->
+<?php deleteCategories();?>
 
-}
-         
-?>
-                              
-                            
-                              
-                               
-                           </tbody>
-                       </table>
-                       
-                       
-                       
-                        </div>
 
-            
-            
-            
-            
-            
 
-        </div>
-        <!-- /#page-wrapper -->
 
-   <?php include "includes/admin_footer.php";?>
+
+
+</tbody>
+</table>
+
+
+
+</div>
+
+
+
+
+
+
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php";?>
