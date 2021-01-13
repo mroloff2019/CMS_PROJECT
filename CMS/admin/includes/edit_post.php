a15a31e14e3862df89294120ec926d123c75eef1
mroloff <mroloff.dev@gmail.com>
2021-01-13 09:28:48 -0800

    Edit_post.php updated
    After user edits post, it will not be reflected to all posts.
    
    Database updates

--- CMS/admin/includes/edit_post.php
+++ CMS/admin/includes/edit_post.php
@@ -14,7 +14,7 @@ If(isset($_GET['p_id'])) {
 }
 
 
-$query = "SELECT * FROM posts";
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
 $select_posts_by_id = mysqli_query($connection,$query);
 
 
@@ -29,8 +29,57 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
         $post_tags = $row['post_tags']; 
         $post_comment_count = $row['post_comment_count']; 
         $post_date = $row['post_date']; 
-
+        
 }
+
+if(isset($_POST['update_post']))    {
+       
+        $post_author = $_POST['post_author'];
+        $post_title = $_POST['post_title'];
+        $post_category_id = $_POST['post_category'];
+        $post_status = $_POST['post_status'];
+        $post_image = $_FILES['image']['name'];
+        $post_image_temp = $_FILES['image']['tmp_name'];
+        $post_content = $_POST['post_content'];
+        $post_tags = $_POST['post_tags'];
+    
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+        if(empty($post_image)) {
+        
+            $query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+            $select_image = mysqli_query($connection, $query);
+            
+            while($row = mysqli_fetch_array($select_image)) {
+                
+                $post_image=$row['post_image'];
+            }
+            
+        }
+    
+           $query = "UPDATE posts SET ";
+           $query .="post_title  = '{$post_title}', ";
+           $query .="post_category_id = '{$post_category_id}', ";
+           $query .="post_date  = now(), ";
+           $query .="post_author  = '{$post_author}', ";
+           $query .="post_status  = '{$post_status}', ";
+           $query .="post_tags  = '{$post_tags}',";
+           $query .="post_content = '{$post_content}', ";
+           $query .="post_image = '{$post_image}' ";
+           $query .= "WHERE post_id = {$the_post_id} ";
+    
+        $update_post = mysqli_query($connection, $query);
+    
+ confirm($update_post);
+    
+    }
+    
+   
+   
+           
+           
+
+
+
 ?>
    
 
@@ -45,13 +94,13 @@ while($row = mysqli_fetch_assoc($select_posts_by_id)) {
     
 <div class="form-group">
     <label for="title">Post Title</label>
-    <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+    <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     
 </div>
    
    
    <div class="form-group">
-       <select name="post_category" id="post_category">
+       <select name="post_category" id="">
            
 <?php 
 $query = "SELECT * FROM categories";
@@ -68,6 +117,9 @@ $cat_title = $row['cat_title'];
     
     
 }
+           
+   
+           
            
 ?>
 
@@ -95,7 +147,7 @@ $cat_title = $row['cat_title'];
     
     <div class="form-group">
         <label for="title">Post Author</label>
-        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
         
     </div>
     
@@ -106,7 +158,8 @@ $cat_title = $row['cat_title'];
     </div>
     
     <div class="form-group">
-       <img width="100" src="../images<?php echo $post_image?> " alt="">
+       <img width="100" src="../images <?php echo $post_image?> " alt="">
+       <input type="file" name="image">
        
         
     </div>
@@ -130,7 +183,7 @@ $cat_title = $row['cat_title'];
     
     <div class="form-group">
         
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update post">
         </div>
     
     
